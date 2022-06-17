e8b0dd6fd03eb215d7f33524a9d9da41027d779f
Colt Skeen <colton.skeen@gmail.com>
2022-06-16 23:09:44 -0400

    updated markup for annotation changes

--- RazorCountry/Models/Continent.cs
+++ RazorCountry/Models/Continent.cs
@@ -1,9 +1,22 @@
-﻿namespace RazorCountry.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorCountry.Models
 {
     public class Continent
     {
+        [Required, StringLength(2, MinimumLength = 2), Display(Name = "Code")] 
+        [RegularExpression(@"[A-Z]+", ErrorMessage = "Only upper case characters are allowed.")]
         public string ID { get; set; }
+
+        [Required] 
         public string Name { get; set; }
+
+        /* Annotations Explaination
+         * [Required] --> requires a a field to contain a value 
+         * [StringLength(2, MinimumLength = 2) --> forces a 2 character input
+         * [Display(Name = "Code")] --> Makes the display name more friendly
+         * [RegularExpression ... ] --> Accepts only uppercase input and returns en error otherwise
+         */
     }
 }
 
